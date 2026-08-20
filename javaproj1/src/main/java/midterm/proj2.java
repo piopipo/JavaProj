fbf517f8e44a0c8357f143bdf498bf853473fa3e
piopipo <eugenepiolos@gmail.com>
2026-08-20 20:01:40 +0800

    Added Prelim proj2

---
+++ javaproj1/src/main/java/midterm/proj2.java
@@ -0,0 +1,22 @@
+
+package midterm;
+
+import java.util.Scanner;
+
+public class proj2 {
+   public static void main(String[] args) { Scanner scanner = new Scanner(System.in);
+   
+   System.out.print("Enter a: ");
+   
+   int a = scanner.nextInt();
+   
+   System.out.print("Enter b: ");
+   
+   int b = scanner.nextInt();
+   
+   System.out.println("Sum is: " + (a + b));
+   
+   scanner.close(); 
+   } 
+} 
+
